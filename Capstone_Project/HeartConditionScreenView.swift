fafac314a9bbdf5b7857666820f614fe6f8b624a
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-03-06 18:03:27 -0500

    Adding computed blood pressure value infomation on Heart Condition Screen

---
+++ Capstone_Project/HeartConditionScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HeartConditionScreenView.swift
+//  Capstone_Project
+//
+//  Created by Emeka Ekeke on 2024-03-06.
+//
+
+import SwiftUI
+
+struct HeartConditionScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeartConditionScreenView()
+}
