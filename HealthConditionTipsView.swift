1f9925546720ff3b7110fdb5bf02df32c64c2f04
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-03-06 23:29:33 -0500

    Adding the Health Condition Tip Screen and passing Relevent health condition  data to it.

---
+++ HealthConditionTipsView.swift
@@ -0,0 +1,18 @@
+//
+//  HealthConditionTipsView.swift
+//  Capstone_Project
+//
+//  Created by Emeka Ekeke on 2024-03-06.
+//
+
+import SwiftUI
+
+struct HealthConditionTipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthConditionTipsView()
+}
