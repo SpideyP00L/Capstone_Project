c939ae241f20ead9433093f60ac7460dd4dfd692
Jeet Panchal <jeetbpanchal.com>
2024-03-12 19:38:31 -0400

    Release 2 Sprint 2 Final Changes

---
+++ Capstone_Project/HeartConditionTipsView.swift
@@ -0,0 +1,18 @@
+//
+//  HeartConditionTipsView.swift
+//  Capstone_Project
+//
+//  Created by Jeet Panchal on 2024-03-12.
+//
+
+import SwiftUI
+
+struct HeartConditionTipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeartConditionTipsView()
+}
